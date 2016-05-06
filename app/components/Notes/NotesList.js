2c1fc459470a8b91852a3226097a153a416d4a94
jortiz <josepho011235@gmail.com>
2016-05-06 00:06:41 -0400

    refactored list item

--- app/components/Notes/NotesList.js
+++ app/components/Notes/NotesList.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
 class NotesList extends React.Component {
+	
 	render(){
+		const { notes } = this.props;
 		return (
 			<ul className="list-group">
-				{this.props.notes.map(note,index) => { return <li className="list-group-item" key={index}>{note['.value']}<li>
-			}}
+				{notes.map((note, index) => (
+					<li className="list-group-item" key={index}>{note['.value']}</li>
+				))}
 			</ul>
 		)
 	}
-};
-
+}
 
 export default NotesList
+
+
--- public/bundle.js
+++ public/bundle.js
@@ -25912,7 +25912,7 @@
 	'use strict';
 
 	var React = __webpack_require__(1);
-	var NotesList = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./NotesList\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var NotesList = __webpack_require__(235);
 	var AddNote = __webpack_require__(236);
 
 	var Notes = React.createClass({
@@ -25944,7 +25944,63 @@
 	module.exports = Notes;
 
 /***/ },
-/* 235 */,
+/* 235 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var NotesList = function (_React$Component) {
+		_inherits(NotesList, _React$Component);
+
+		function NotesList() {
+			_classCallCheck(this, NotesList);
+
+			return _possibleConstructorReturn(this, Object.getPrototypeOf(NotesList).apply(this, arguments));
+		}
+
+		_createClass(NotesList, [{
+			key: "render",
+			value: function render() {
+				var notes = this.props.notes;
+
+				return _react2.default.createElement(
+					"ul",
+					{ className: "list-group" },
+					notes.map(function (note, index) {
+						return _react2.default.createElement(
+							"li",
+							{ className: "list-group-item", key: index },
+							note['.value']
+						);
+					})
+				);
+			}
+		}]);
+
+		return NotesList;
+	}(_react2.default.Component);
+
+	exports.default = NotesList;
+
+/***/ },
 /* 236 */
 /***/ function(module, exports, __webpack_require__) {
 
